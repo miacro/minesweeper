f470983f1230ec97c4f01fe94567c42679af0245
miacro <fqguozhou@gmail.com>
2017-03-28 23:18:40 +0800

    ..

---
+++ js/mine-matrix/mine-matrix.js
@@ -0,0 +1,16 @@
+class MineMatrix {
+  constructor(width, height, mineCount) {
+    this.matrix = [];
+    for (var i = 0; i < height; ++i) {
+      var row = [];
+      for (var j = 0; j < width; ++j) {
+        row.push(false);
+      }
+      this.matrix.push(row);
+    }
+    var totalCount = width * height;
+  
+  };
+
+};
+export MineMatrix;
