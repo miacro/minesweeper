4ceb2f0766f2d348714d6674c916d1d8c07c59de
miacro <fqguozhou@gmail.com>
2017-03-28 22:30:17 +0800

    ...

--- js/component/minefield.js
+++ js/component/minefield.js
@@ -1,8 +1,23 @@
 class Minefield extends React.Component {
-    render() {
-        return (
-            <div>
-              hello world
-            </div>);
+  render() {
+    var dimension = {width: 30, height: 16};
+    if (this.props.dimension) {
+      if (this.props.dimension.width != undefined) {
+        dimension.width = this.props.dimension.width;
+        }
+      if (this.props.dimension.height != undefined) {
+        dimension.height = this.props.dimension.height;
+      }
+      }
+    var columns = [];
+    for (var i = 0; i < dimension.height; ++i) {
+      var row = [];
+      for (var j = 0; j < dimension.width; ++j) {
+        row.push(<Cell />);
+      }
+      columns.push(
+        <div>{row}</div>);
+      }
+      return (<div>{columns}</div>);
     };
-};
+  };
